70b3c8d506060466a6ab4f550597e65cf08fdb23
Elucode <125608503+Elulove1@users.noreply.github.com>
2024-08-12 22:23:02 -0400

    Post request

--- server/routes.js
+++ server/routes.js
@@ -17,3 +17,18 @@ router.get("/todos", async (req, res) => {
     res.status(200).json(todos);
 });
 
+// POST /todos
+router.post("/todos", async (req, res) => {
+    const collection = getCollection();
+    let { todo } = req.body;
+
+    if (!todo) {
+        return res.status(400).json({ mssg: "error no todo found" });
+    }
+
+    todo = (typeof todo === "string") ? todo : JSON.stringify(todo);
+
+    const newTodo = await collection.insertOne({ todo, status: false });
+
+    res.status(201).json({ todo, status: false, _id: newTodo.insertedId });
+});
