d172acf8c5182c93b903a00ccba58e23b8a3d428
Elucode <125608503+Elulove1@users.noreply.github.com>
2024-08-12 22:22:05 -0400

    get request/API

--- server/routes.js
+++ server/routes.js
@@ -8,3 +8,12 @@ const getCollection = () => {
     const collection = client.db("todosdb").collection("todos");
     return collection;
 }
+
+// GET /todos
+router.get("/todos", async (req, res) => {
+    const collection = getCollection();
+    const todos = await collection.find({}).toArray();
+
+    res.status(200).json(todos);
+});
+
