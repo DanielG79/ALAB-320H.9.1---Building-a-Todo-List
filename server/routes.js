3de6b0b8295e972f227daddb99492c9e5cb6cfac
Elucode <125608503+Elulove1@users.noreply.github.com>
2024-08-12 22:19:31 -0400

    routes.js created

---
+++ server/routes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const { getConnectedClient } = require("./database");
+const { ObjectId } = require("mongodb");
+
+const getCollection = () => {
+    const client = getConnectedClient();
+    const collection = client.db("todosdb").collection("todos");
+    return collection;
+}
